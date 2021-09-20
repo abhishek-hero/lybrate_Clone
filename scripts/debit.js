820eeaf1d263aa9c78763e027dff1dbbcfe5bd5b
Abhishek M <87421921+abhishek-hero@users.noreply.github.com>
2021-09-20 23:27:17 +0530

    Update debit.js

--- scripts/debit.js
+++ scripts/debit.js
@@ -1,10 +1,23 @@
-let submit = document.getElementById("submit")
-let Price = document.getElementById('total');
+// let submit = document.getElementById("submit")
+// let Price = document.getElementById('total');
 
-let totalDetaild= JSON.parse(localStorage.getItem('totalPrice'));
+// let totalDetaild= JSON.parse(localStorage.getItem('totalPrice'));
 
-Price.textContent=`Total Amount Payable :  ₹${totalDetaild.totalPrice}`;
+// Price.textContent=`Total Amount Payable :  ₹${totalDetaild.totalPrice}`;
 
 // submit.onclick = function (){
 //     window.location.href = "success.html"
 // }
+
+function sucess(){
+    window.location.href = "success.html"
+}
+
+let add = JSON.parse(localStorage.getItem("totalPrice"))
+
+function total(){
+    let heading = document.getElementById("total_payable")
+    heading.innerHTML = `Total Payable Amount  - ₹ ${add.totalPrice}`
+}
+
+total()
