4bf5ba7c7dc22defd00094c4c7374dd521d4c375
Abhishek M <87421921+abhishek-hero@users.noreply.github.com>
2021-09-20 23:07:26 +0530

    Update debit.js

--- scripts/debit.js
+++ scripts/debit.js
@@ -6,10 +6,5 @@ let totalDetaild= JSON.parse(localStorage.getItem('totalPrice'));
 Price.textContent=`Total Amount Payable :  ₹${totalDetaild.totalPrice}`;
 
 submit.onclick = function (){
-    let data = document.getElementByClassName("required").value;
-    if(data.length == 0){
-alert("Fill all credentials")
-    }else {
-window.location.href = "success.html"
-    }
+    window.location.href = "success.html"
 }
